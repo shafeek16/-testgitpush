6491100389faf3ad2b59d3b1874f3946b0ebb8d3
shafeek <98shafeek16@gmail.com>
2022-08-27 23:26:25 +0530

    this is my first code

---
+++ mongotest.py
@@ -0,0 +1,13 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://chhotu:bhootnagar@cluster0.osd6scd.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name":"shafeek"
+    "email"'shfkzhr@gmail.com'
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
